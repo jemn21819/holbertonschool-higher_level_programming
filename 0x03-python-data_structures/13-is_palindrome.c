f330d1a6664ee1c922b5181603b822e5c7edfe16
Jaime Martinez <jemn1jemn@gmail.com>
2020-12-16 00:56:05 +0000

    adding tasks files

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,35 +1,28 @@
 #include "lists.h"
-
-/**
- * linkedlist_pal - verify if a linked list can be palindrome
- * @tmp: temporal head
- * @h: head of the list
- * Return: 1 if it is a linked list, 0 if not
- */
-
-int linkedlist_pal(listint_t **tmp, listint_t *h)
-{
-	if (!h)
-		return (1);
-	while (linkedlist_pal(tmp, h->next) == 1 && (*tmp)->n == h->n)
-	{
-		*tmp = (*tmp)->next;
-		return (1);
-	}
-	return (0);
-}
-
-/**
- * is_palindrome - checks if a singly linked list is a palindrome
- * @head: head of the linked list
- * Return: 0 if it is not a palindrome, 1 if it is a palindrome
- */
+#include <stdlib.h>
 
 int is_palindrome(listint_t **head)
 {
-	int p;
+	int size;
+	int options[2048];
+	int i;
 
-	if (!head || !*head || !(*head)->next)
+	size = 0;
+	if (head == NULL || *head == NULL)
 		return (1);
-	p = linkedlist_pal(head, *head);
-	return (p);
+
+	while(*head != NULL)
+	{
+		size++;
+		options[size - 1] = (*head)->n;
+		head = &(*head)->next;
+	}
+
+	for (i = 0; i < size / 2; i++)
+	{
+		if (options[i] != options[size - i - 1])
+			return (0);
+	}
+
+	return (1);
+}
