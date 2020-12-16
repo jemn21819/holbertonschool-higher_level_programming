af19d1119065215c26241e1571fc4eabaa3d94ba
Jaime Martinez <jemn1jemn@gmail.com>
2020-12-16 00:31:22 +0000

    adding tasks files

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome.
+ * @head: double pointer to head of list
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+*/
+
+int is_palindrome(listint_t **head)
+{
+	listint_t *current;
+	size_t size, i, flag = 1;
+	int *pal_arr;
+
+	if (*head == NULL)
+		return (1);
+	current = *head;
+	for (size = 0; current; size++)
+		current = current->next;
+	pal_arr = malloc(sizeof(int) * size);
+	if (!pal_arr)
+		return (1);
+	current = *head;
+	for (i = 0; i < size; i++)
+	{
+		pal_arr[i] = current->n;
+		current = current->next;
+	}
+	for (i = 0; i < size; i++, size--)
+	{
+		if (pal_arr[i] != pal_arr[size - 1])
+			flag = 0;
+	}
+	free(pal_arr);
+	if (flag == 0)
+		return (0);
+	return (1);
+}
