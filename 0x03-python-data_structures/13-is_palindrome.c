762d9c067ca1c24eb1f3bd6b64dec93395098e05
Jaime Martinez <jemn1jemn@gmail.com>
2020-12-16 00:58:55 +0000

    adding tasks files

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -4,23 +4,23 @@
 int is_palindrome(listint_t **head)
 {
 	int size;
-	int options[2048];
+	int optns[2048];
 	int i;
 
 	size = 0;
-	if (head == NULL || *head == NULL)
+	if (!head || !*head)
 		return (1);
 
 	while(*head != NULL)
 	{
 		size++;
-		options[size - 1] = (*head)->n;
+		optns[size - 1] = (*head)->n;
 		head = &(*head)->next;
 	}
 
 	for (i = 0; i < size / 2; i++)
 	{
-		if (options[i] != options[size - i - 1])
+		if (optns[i] != optns[size - i - 1])
 			return (0);
 	}
 
