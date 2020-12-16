d267dbe500fcc303c569fcb3d3c409c9918f5a71
Jaime Martinez <jemn1jemn@gmail.com>
2020-12-16 00:53:53 +0000

    adding tasks files

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,40 +1,35 @@
 #include "lists.h"
-#include <stdlib.h>
-#include <stdio.h>
 
 /**
- * is_palindrome - checks if a singly linked list is a palindrome.
- * @head: double pointer to head of list
+ * linkedlist_pal - verify if a linked list can be palindrome
+ * @tmp: temporal head
+ * @h: head of the list
+ * Return: 1 if it is a linked list, 0 if not
+ */
+
+int linkedlist_pal(listint_t **tmp, listint_t *h)
+{
+	if (!h)
+		return (1);
+	while (linkedlist_pal(tmp, h->next) == 1 && (*tmp)->n == h->n)
+	{
+		*tmp = (*tmp)->next;
+		return (1);
+	}
+	return (0);
+}
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: head of the linked list
  * Return: 0 if it is not a palindrome, 1 if it is a palindrome
-*/
+ */
 
 int is_palindrome(listint_t **head)
 {
-	listint_t *current;
-	size_t size, i, flag = 1;
-	int *pal_arr;
+	int p;
 
-	if (*head == NULL)
+	if (!head || !*head || !(*head)->next)
 		return (1);
-	current = *head;
-	for (size = 0; current; size++)
-		current = current->next;
-	pal_arr = malloc(sizeof(int) * size);
-	if (!pal_arr)
-		return (1);
-	current = *head;
-	for (i = 0; i < size; i++)
-	{
-		pal_arr[i] = current->n;
-		current = current->next;
-	}
-	for (i = 0; i < size; i++, size--)
-	{
-		if (pal_arr[i] != pal_arr[size - 1])
-			flag = 0;
-	}
-	free(pal_arr);
-	if (flag == 0)
-		return (0);
-	return (1);
-}
+	p = linkedlist_pal(head, *head);
+	return (p);
