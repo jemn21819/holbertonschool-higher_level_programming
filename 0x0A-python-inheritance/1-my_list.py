64b2d37e7e7e0c8174926f0525dfaf09b585fa40
James Nieves <jmartinez21734@stu.nuc.edu>
2021-01-20 01:54:34 +0000

    adding tests files

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,6 +6,10 @@ Module contain class Mylist
 
 class MyList(list):
     """MyList class"""
+    def __init__(self):
+        """initializes the object"""
+        super().__init__()
+
     def print_sorted(self):
         """ function print sorted list"""
         print(sorted(self))
