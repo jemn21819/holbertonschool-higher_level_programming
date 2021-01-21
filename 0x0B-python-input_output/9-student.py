7314e2a2a52925530011ccc0ae163f6a47265d07
James Nieves <jmartinez21734@stu.nuc.edu>
2021-01-21 01:08:45 +0000

    adding

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-"""
-Module contain class Student
-"""
+"""Module class Student"""
 
 
 class Student:
-    """Class for student"""
+    """Class to represent student"""
     def __init__(self, first_name, last_name, age):
-        """initialize student"""
+        """Initialize new student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
         """to_json"""
-        return self.__dict___
+        return self.__dict__.copy()
