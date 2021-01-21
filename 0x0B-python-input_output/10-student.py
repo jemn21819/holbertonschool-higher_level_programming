20903c65c07c1aea4c63988242a46d420bfd2ef6
James Nieves <jmartinez21734@stu.nuc.edu>
2021-01-21 01:11:00 +0000

    adding

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-"""
-Module contain class Student
-"""
+"""Module class Student"""
 
 
 class Student:
-    """Class for student"""
+    """Class to represent student"""
     def __init__(self, first_name, last_name, age):
-        """initialize student"""
+        """Initialize new student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
