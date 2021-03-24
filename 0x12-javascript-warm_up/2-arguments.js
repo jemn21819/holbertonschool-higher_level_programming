c979ac6b3769a4cbd8fd915b6018be6cb79e2e4c
James <jmartinez21834@stu.nuc.edu>
2021-03-24 04:06:31 +0000

    adadf

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
-const args = process.argv.length;
-if (args <= 2) {
+'use strict';
+const l = process.argv.length;
+if (l === 2) {
   console.log('No argument');
-} else if (args === 3) {
+} else if (l === 3) {
   console.log('Argument found');
 } else {
-  console.log('Argument found');
+  console.log('Arguments found');
 }
