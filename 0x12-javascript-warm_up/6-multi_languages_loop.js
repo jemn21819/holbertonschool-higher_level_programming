debc8ffe51834006d7aa9342f45bdb1bd2d3bb3a
James <jmartinez21834@stu.nuc.edu>
2021-03-24 03:52:32 +0000

    adding taks files

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
-const line = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let i = 0; i < line.length; i++) {
-  console.log(line[i]);
+const list = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+let i = 0;
+while (i < list.length) {
+  console.log(list[i]);
+  i++;
 }
