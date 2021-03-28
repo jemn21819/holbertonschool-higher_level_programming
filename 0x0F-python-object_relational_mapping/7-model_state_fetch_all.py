cbeff8a18b8af835cc7b016cc8f35a0c2ac43f71
James <jmartinez21834@stu.nuc.edu>
2021-03-28 11:06:26 +0000

    adding task 7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,4 +1,4 @@
-#!/usr.bin/python3
+#!/usr/bin/python3
 """
 script that lists all State objects from the database hbtn_0e_6_usa
 """
@@ -13,7 +13,6 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://'
                            '{}:{}@localhost/{}'
                            .format(argv[1], argv[2], argv[3]))
-    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
     for state in session.query(State).order_by(Sate.id):
