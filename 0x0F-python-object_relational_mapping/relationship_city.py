d7511454fa987c824b9d7835697984a1021d710d
James <jmartinez21834@stu.nuc.edu>
2021-03-28 12:43:19 +0000

    adding files for task 15

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Create table `cities`"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from relationship_state import Base
+
+
+class City(Base):
+    """Class representing the `cities` table"""
+
+    __tablename__ = 'cities'
+
+    id = Column(Integer, nullable=False, primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
