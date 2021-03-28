87b8ebe78bc684c61e99f25e5dcb9e205a56aefa
James <jmartinez21834@stu.nuc.edu>
2021-03-28 11:16:14 +0000

    adding task 7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
 
     engine = create_engine('mysql://{}:{}@{}:{}/{}'.format(
         argv[1], argv[2], 'localhost', 3306, argv[3]))
-    session = sessionmaker(engine)().query(State).order_by(Sate.id)
+    session = sessionmaker(engine)().query(State).order_by(State.id)
     for state in session:
         print("{}: {}".format(state.id, state.name))
     sessiom.close()
