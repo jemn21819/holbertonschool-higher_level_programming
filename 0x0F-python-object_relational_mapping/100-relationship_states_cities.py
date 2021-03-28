cec445c6911c606e8051a3120d09e78e120c1993
James <jmartinez21834@stu.nuc.edu>
2021-03-28 12:56:53 +0000

    fixing things

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -7,14 +7,12 @@ import sqlalchemy
 from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
-
+from relationship_state import Base, State
+from relationship_city import City
 if __name__ == "__main__":
 
     engine = create_engine('mysql://{}:{}@{}:{}/{}'.format(
         argv[1], argv[2], 'localhost', 3306, argv[3]))
-
-    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
 
