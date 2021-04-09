17f2837ed823d2dbcd65f537687496335edbf3dd
James <jmartinez21834@stu.nuc.edu>
2021-04-09 02:04:47 +0000

    adding task 6 files

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,5 +6,6 @@ finds a peak in a list of unsorted integers
 
 def find_peak(list_of_integers):
     """finds a peak in a list of unsorted integers"""
-    if len(list_of_integers) != 0:
-        return(max(list_of_integers))
+    if len(list_of_integers) == 0:
+        return None
+    return(max(list_of_integers))
