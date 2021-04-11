6ffa9852fb63204291da31eb603a9f9a292a9080
James <jmartinez21834@stu.nuc.edu>
2021-04-11 12:33:02 +0000

    fixin pe8 white space

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 takes in a URL and an email, sends a POST request to the passed URL with the
-email as a parameter, and displays the body of the response 
+email as a parameter, and displays the body of the response
 """
 import urllib.request as req
 import urllib.parse as parse
