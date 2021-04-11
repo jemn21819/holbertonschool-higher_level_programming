e3cb8a5f4a0d6186459af1c9f584b3f44b7d14ee
James <jmartinez21834@stu.nuc.edu>
2021-04-11 14:25:20 +0000

    ading task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter.
+"""
+import requests as req
+from sys import argv
+
+if __name__ == "__main__":
+    url = 'http://0.0.0.0:5000/search_user'
+    q = argv[1] if len(argv) > 1 else ""
+    res = req.post(url, data={'q': q})
+    try:
+        res_dic = res.json()
+        if res_dic == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(r_dict.get('id'), r_dict.get('name')))
+    except:
+        print("Not a valid JSON")
