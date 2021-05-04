ba962fc31a4b745947eceb3c3e2d80c073d47b27
James <jmartinez21834@stu.nuc.edu>
2021-05-04 21:16:44 +0000

    fixin typo

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -7,7 +7,7 @@ request.get(process.argv[2], function (err, data, body) {
     let times = 0;
     const movies = JSON.parse(body).results;
     for (const actor in movies) {
-      const character = movies[actor].character;
+      const character = movies[actor].characters;
       for (const i in character) {
         if (character[i].includes('18')) {
           times++;
